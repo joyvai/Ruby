e5c4ce3b2bd24ab8c4e2b501478dee0c6df5bc05
iftekharjoy <iftekharjoy1@gmail.com>
2017-05-12 23:44:53 +0600

    true and false check

---
+++ code.rb
@@ -0,0 +1,31 @@
+print "Enter a Value: "
+
+# to_i is to_integer it's take the string variable
+# convert into int
+first_num = gets.to_i
+
+print "Enter Another Value: "
+
+second_num = gets.to_i
+# to_s means string
+# it's converts int to string
+puts first_num.to_s + " +  " + second_num.to_s + " = "+(first_num+second_num).to_s
+
+big_float = 1.1234893439473949347
+
+puts(big_float + 0.000000000000005)
+
+a = 1.1
+b = 2.1
+puts (a+b)
+
+
+
+
+
+
+
+
+
+
+
---
+++ true_false.rb
@@ -0,0 +1,4 @@
+puts "true && false = " + (true && false).to_s
+puts "true || false = " + (true || false).to_s
+puts "!false = " + (!false).to_s
+puts "!true = " +(!true).to_s 
