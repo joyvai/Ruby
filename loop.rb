46250164c9109bb45f6d6db99c923c140af55195
iftekharjoy <iftekharjoy1@gmail.com>
2017-05-13 00:45:16 +0600

    looping

---
+++ loop.rb
@@ -0,0 +1,44 @@
+# x = 1
+
+# loop do 
+# 	x +=1
+
+# 	next unless (x % 2) ==0
+# 	puts x		
+# 	break if x>=10
+# end
+
+# while loop
+
+# y = 10
+# while y <= 20
+# 	y+=1 
+# 	# if you use "if " it doesn't work 
+# 	next unless(y %2) ==0	
+# 	puts y		
+# end
+
+# a = 1
+# u ntil a >= 10
+# 	a += 1
+# 	next unless (a % 2) == 0
+# 	puts a
+# end
+
+# numbers = [1,2,3,4,5]
+
+# for number in numbers 
+# 	print "#{number}, "
+# end
+
+groceries = ["bananas","sweet potato", "tomato","pasta"]
+
+groceries.each do | food|
+	puts "Get Some #{food}"
+end
+
+
+
+
+
+
