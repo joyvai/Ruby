91b0df0a0694b1cee550d7a925d3e5f6946bf3c0
iftekharjoy <iftekharjoy1@gmail.com>
2017-05-12 23:51:37 +0600

    hello World

---
+++ hashes.rb
@@ -0,0 +1,90 @@
+number_hash = {
+	"PI" => 3.14,
+	"Golden" => 1.618,
+	"e"  => 2.718
+}
+
+puts number_hash["PI"]
+
+
+superheroes = Hash["Clark Kent","Superman","Bruce Wayne","Batman" ]
+
+puts superheroes["Clark Kent"] 
+
+superheroes["Barry Allen"] = "Flash"
+puts superheroes
+superheroines = Hash["Lisa Morel","Aquagirl","Betty Kane","Batgir"]
+
+superheroes.update(superheroines)
+
+superheroes.each do |key,value|
+	puts key.to_s + " : " + value
+end
+
+
+
+instSection ={
+	'cello' => 'string',
+	'clarinet' => 'woodwind',
+	'drum' => 'percussion',
+	'oboe' => 'woodwind',
+	'trumpet' => 'brass',
+	'violin' => 'string' 
+}
+
+puts instSection['oboe']
+
+
+puts instSection['cello']
+
+line =  gets
+print line
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
