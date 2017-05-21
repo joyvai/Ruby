a5348685016b0708976fc3cc838be13b6ee3c1aa
iftekharjoy <iftekharjoy1@gmail.com>
2017-05-21 23:45:07 +0600

    module

---
+++ module.rb
@@ -0,0 +1,33 @@
+module Add
+	def addTwoNumber(first_num,second_num)
+		puts first_num + second_num
+	end
+end
+
+module description
+	your_name = gets
+	puts 'My name is #{your_name}'
+	your_age = gets.to_i
+	puts "My age #{your_age}"
+	puts "Are you Male or Female"
+	gender = gets
+	if (gender === 'Male' or gender== 'M' or gender=='m' or )
+class Myself
+	include Add
+	def age 
+		puts "my age is 19"
+	end
+end
+
+class Music
+	include Add
+	def song
+		puts 'fix you'
+	end
+end
+
+my = Myself.new
+mu = Music.new
+my.addTwoNumber(2,4)
+my.age	
+
