5d3537602c9eff2e7118eceef0f815cf142774dd
iftekharjoy <iftekharjoy1@gmail.com>
2017-05-23 23:29:18 +0600

    modified hashes

--- hashes.rb
+++ hashes.rb
@@ -1,52 +1,59 @@
-number_hash = {
-	"PI" => 3.14,
-	"Golden" => 1.618,
-	"e"  => 2.718
-}
+# number_hash = {
+# 	"PI" => 3.14,
+# 	"Golden" => 1.618,
+# 	"e"  => 2.718
+# }
 
-puts number_hash["PI"]
+# puts number_hash["PI"]
 
 
-superheroes = Hash["Clark Kent","Superman","Bruce Wayne","Batman" ]
+# superheroes = Hash["Clark Kent","Superman","Bruce Wayne","Batman" ]
 
-puts superheroes["Clark Kent"] 
+# puts superheroes["Clark Kent"] 
 
-superheroes["Barry Allen"] = "Flash"
-puts superheroes
-superheroines = Hash["Lisa Morel","Aquagirl","Betty Kane","Batgir"]
+# superheroes["Barry Allen"] = "Flash"
+# puts superheroes
+# superheroines = Hash["Lisa Morel","Aquagirl","Betty Kane","Batgir"]
 
-superheroes.update(superheroines)
+# superheroes.update(superheroines)
 
-superheroes.each do |key,value|
-	puts key.to_s + " : " + value
+# superheroes.each do |key,value|
+# 	puts key.to_s + " : " + value
+# end
+
+
+
+# instSection ={
+# 	'cello' => 'string',
+# 	'clarinet' => 'woodwind',
+# 	'drum' => 'percussion',
+# 	'oboe' => 'woodwind',
+# 	'trumpet' => 'brass',
+# 	'violin' => 'string' 
+# }
+
+# puts instSection['oboe']
+
+
+# puts instSection['cello']
+
+# line =  gets
+# print line
+
+
+
+
+
+treehouse = [ 
+
+	"location" => "Usa",
+	"Business_name" => "treehouse",
+	"employee" => 20
+]
+
+treehouse.each do |key,value|
+	puts "The name #{key} at #{value}"
 end
-
-
-
-instSection ={
-	'cello' => 'string',
-	'clarinet' => 'woodwind',
-	'drum' => 'percussion',
-	'oboe' => 'woodwind',
-	'trumpet' => 'brass',
-	'violin' => 'string' 
-}
-
-puts instSection['oboe']
-
-
-puts instSection['cello']
-
-line =  gets
-print line
-
-
-
-
-
-
-
-
 
 
 
