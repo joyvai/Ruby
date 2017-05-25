186a708389650bcb2fb7eaf11d00a228fb54125f
iftekharjoy <iftekharjoy1@gmail.com>
2017-05-25 23:14:26 +0600

    symbols add

--- Time/symbols.rb
+++ Time/symbols.rb
@@ -2,3 +2,9 @@ current_situation = :good
 
 puts "Everything is fine" if current_situation == :good
 puts "PANIC!" if current_situation == :bad
+
+# :good and :bad are symbols
+# symbols take less memory than strings
+# if we use "good" or "bad" strings 
+# result will be same
+# But symbols initialized once.
