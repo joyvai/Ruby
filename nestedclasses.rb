71161b3837a4f2ef1a469f652a294ba3084e7feb
iftekharjoy <iftekharjoy1@gmail.com>
2017-06-02 21:04:40 +0600

    nested class code add

---
+++ nestedclasses.rb
@@ -0,0 +1,24 @@
+# nested classes
+class Drawing
+	def Drawing.give_me_a_circle
+		Circle.new
+	end
+
+	class Line
+	end
+
+	class Circle
+		def what_am_i
+			"This is a circle"
+		end
+	end
+end
+
+a = Drawing.give_me_a_circle
+puts a.what_am_i
+a = Drawing::Circle.new
+puts a.what_am_i
+# This method doesn't exist
+# Circle does not exist.
+a = Circle.new
+puts a.what_am_i
