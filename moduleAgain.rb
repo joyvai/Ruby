d16f75d8ec7e44cf6a8d2d7b4dfeec4c65577b90
iftekharjoy <iftekharjoy1@gmail.com>
2017-06-02 21:03:37 +0600

    module

---
+++ moduleAgain.rb
@@ -0,0 +1,15 @@
+module NumberStuff
+	def NumberStuff.random
+		rand(1000000)
+	end
+end
+
+module LetterStuff
+	def LetterStuff.random
+		(rand(26) + 65).chr
+	end
+end
+
+puts NumberStuff.random
+puts LetterStuff.random
+
