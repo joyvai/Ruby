a9d4e8e357bd4a29101ea220f89bbee933294185
iftekharjoy <iftekharjoy1@gmail.com>
2017-06-02 19:31:03 +0600

    add protected example

---
+++ protected.rb
@@ -0,0 +1,22 @@
+# you can call a protected method
+# same class any objects
+class Person 
+	def initialize(age)
+		@age = age
+	end
+
+	def age 
+		@age 
+	end
+
+	def age_difference_with(other_person)
+		(self.age - other_person.age).abs
+	end
+
+	protected :age
+end
+
+fred = Person.new(34)
+chris = Person.new(25)
+puts chris.age_difference_with(fred)
+puts chris.age
